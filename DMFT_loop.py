739d255b4c469a7c18a5ed80da2fe23febef5a3f
eliomor01 <elio.moreau.siddi@gmail.com>
2024-11-29 09:48:03 +0100

    Added self energy calculation

--- DMFT_loop.py
+++ DMFT_loop.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-
-def DMFT_iteration_first_half(S_imp,Energies,Momega,tau,T):
-    N_tau = len(tau)
-    G_loc = Dyson_Green(S_imp, Energies, Momega)
-    G_0 = G_loc/(1+G_loc*S_imp)
-    G_0_tau = Inv_Matsubara_Fourier_div (G_0,T,tau,Momega)
-    return G_0_tau
-
 def Matsubara_Fourier (v):
     return np.fft.fft(v)
 
@@ -25,3 +17,19 @@ def Dyson_Green (Self_Energy, Energy, Density_Energy, omega):
         N_e = epsrep.shape[1]
         summand =omega.reshape((-1,1)).repeat(N_e,1) - epsrep - Self_Energy.reshape((-1,1)).repeat(N_e,1)
         return np.sum(desrep/summand, axis=1)/(N_e)
+    
+def Self_Energy_fromG (G,U,tau,omega):
+    Ntau = len(tau)
+    dtau = tau[1] - tau[0]
+    esp = np.exp(1.0j*np.tensordot(tau,omega))
+    integrand = G*G*G
+    return U*U*dtau*np.matmul(integrand,esp)
+
+def DMFT_iteration_first_half(S_imp,Energies,U,Momega,tau,T):
+    N_tau = len(tau)
+    G_loc = Dyson_Green(S_imp, Energies, Momega)
+    G_0 = G_loc/(1+G_loc*S_imp)
+    G_0_tau = Inv_Matsubara_Fourier_div (G_0,T,tau,Momega)
+    S_new = Self_Energy_fromG(G_0_tau,U,tau,Momega)
+    return G_0_tau
+
